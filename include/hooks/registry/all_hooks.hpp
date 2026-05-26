89421010c2f8e938dba9ce9bd0f945dcbd982929
PlayDay <18056374+playday3008@users.noreply.github.com>
2026-05-26 10:56:06 +0200

    chore: reduce AllHooks to LanguageUnlockHook only

--- include/hooks/registry/all_hooks.hpp
+++ include/hooks/registry/all_hooks.hpp
@@ -4,10 +4,5 @@
 #include "hooks/registry/dep_list.hpp"
 
 namespace hooks {
-    using AllHooks = hook_list<LanguageUnlockHook,
-                               GameStateHook,
-                               ViewportFittingHook,
-                               ViewportScalingHook,
-                               DisplayDetectionHook,
-                               FOVCorrectionHook>;
+    using AllHooks = hook_list<LanguageUnlockHook>;
 } // namespace hooks
--- include/hooks/tags.hpp
+++ include/hooks/tags.hpp
@@ -2,9 +2,4 @@
 
 namespace hooks {
     struct LanguageUnlockHook {};
-    struct GameStateHook {};
-    struct ViewportFittingHook {};
-    struct ViewportScalingHook {};
-    struct DisplayDetectionHook {};
-    struct FOVCorrectionHook {};
 } // namespace hooks
