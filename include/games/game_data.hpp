08514996189c887e01af00bbc0a38cd41b5c6876
PlayDay <18056374+playday3008@users.noreply.github.com>
2026-05-30 18:46:45 +0200

    feat(games): add HasVmprotect, HasIntegritySection concepts and game_is_vmprotect variable template

--- include/games/game_data.hpp
+++ include/games/game_data.hpp
@@ -14,4 +14,32 @@ namespace games {
         { game_data<G>::scan_entries };
         typename game_data<G>::ResolvedAddresses;
     };
+
+    template<typename G>
+    concept HasVmprotect = requires {
+        { game_data<G>::vmprotect } -> std::convertible_to<bool>;
+    };
+
+    template<typename G>
+    concept HasIntegritySection = requires {
+        { game_data<G>::integrity_section } -> std::convertible_to<std::string_view>;
+    };
+
+    template<typename G>
+    inline constexpr bool game_is_vmprotect = [] -> bool {
+        if constexpr (HasVmprotect<G>) {
+            return game_data<G>::vmprotect;
+        } else {
+            return false;
+        }
+    }();
+
+    template<typename G>
+    inline constexpr std::string_view game_integrity_section = [] -> std::string_view {
+        if constexpr (HasIntegritySection<G>) {
+            return game_data<G>::integrity_section;
+        } else {
+            return {};
+        }
+    }();
 } // namespace games
