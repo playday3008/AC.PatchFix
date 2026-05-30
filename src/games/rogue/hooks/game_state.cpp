845de487f5cbbfa0a4b5b961cbaf6471d72f0cdc
PlayDay <18056374+playday3008@users.noreply.github.com>
2026-05-30 18:21:10 +0200

    refactor(rogue): migrate GameStateHook to mem module

--- src/games/rogue/hooks/game_state.cpp
+++ src/games/rogue/hooks/game_state.cpp
@@ -2,10 +2,8 @@
 
 #include <atomic>
 
-#include <injector/assembly.hpp>
-#include <injector/injector.hpp>
-
 #include "logger.hpp" // IWYU pragma: keep
+#include "mem/hook.hpp"
 
 #include "games/rogue/game_data.hpp" // IWYU pragma: keep
 
@@ -13,25 +11,33 @@ namespace hooks {
     std::atomic<bool> g_is_in_game {false};
 
     namespace {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wexit-time-destructors"
+#pragma clang diagnostic ignored "-Wglobal-constructors"
+        mem::MidHook g_unpause_hook;
+        mem::MidHook g_pause_hook;
+        mem::MidHook g_pause2_hook;
+#pragma clang diagnostic pop
+
         using G   = games::Rogue;
         using Tag = GameStateHook<G>;
 
         struct GameUnpause {
-            [[maybe_unused]] static void operator()(injector::reg_pack &regs) {
+            [[maybe_unused]] static void operator()(mem::Registers &regs) {
                 g_is_in_game.store(true, std::memory_order_relaxed);
                 *reinterpret_cast<uint8_t *>(regs.rcx + 0x2C0) = 0;
             }
         };
 
         struct GamePause {
-            [[maybe_unused]] static void operator()(injector::reg_pack &regs) {
+            [[maybe_unused]] static void operator()(mem::Registers &regs) {
                 g_is_in_game.store(false, std::memory_order_relaxed);
                 *reinterpret_cast<uint8_t *>(regs.r8 + 0x2C0) = 1;
             }
         };
 
         struct GamePause2 {
-            [[maybe_unused]] static void operator()(injector::reg_pack &regs) {
+            [[maybe_unused]] static void operator()(mem::Registers &regs) {
                 g_is_in_game.store(false, std::memory_order_relaxed);
                 *reinterpret_cast<uint8_t *>(regs.rdi + 0x2C0) = 1;
             }
@@ -43,11 +49,26 @@ namespace hooks {
         log::get()->trace("GameStateHook: installing");
         auto unpause = addrs.game_unpause.value();
         auto pause   = addrs.game_pause.value();
-        injector::MakeInline<GameUnpause>(unpause, unpause + 7);
-        injector::MakeInline<GamePause>(pause, pause + 8);
+        if (auto h = mem::make_hook<GameUnpause>(unpause, unpause + 7)) {
+            g_unpause_hook = std::move(*h);
+        } else {
+            log::get()->error("GameStateHook: unpause hook failed: {}", h.error());
+            return false;
+        }
+        if (auto h = mem::make_hook<GamePause>(pause, pause + 8)) {
+            g_pause_hook = std::move(*h);
+        } else {
+            log::get()->error("GameStateHook: pause hook failed: {}", h.error());
+            return false;
+        }
         if (addrs.game_pause2) {
             auto pause2 = addrs.game_pause2.value();
-            injector::MakeInline<GamePause2>(pause2, pause2 + 7);
+            if (auto h = mem::make_hook<GamePause2>(pause2, pause2 + 7)) {
+                g_pause2_hook = std::move(*h);
+            } else {
+                log::get()->error("GameStateHook: pause2 hook failed: {}", h.error());
+                return false;
+            }
             log::get()->trace("GameStateHook: pause2 hook at 0x{:X}", pause2);
         }
         log::get()->trace("GameStateHook: installed");
