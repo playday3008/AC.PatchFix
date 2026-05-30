be04f56b017f24c0d32093ed37ef67cfde5d8bfe
PlayDay <18056374+playday3008@users.noreply.github.com>
2026-05-30 18:13:42 +0200

    feat(mem): add invoke<Sig>(addr, args...) for game function calls

---
+++ include/mem/call.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstdint>
+
+namespace mem {
+
+    namespace detail {
+        template<typename Sig>
+        struct invoke_impl;
+
+        template<typename Ret, typename... Args>
+        struct invoke_impl<Ret(Args...)> {
+            static auto call(uintptr_t addr, Args... args) -> Ret {
+                return reinterpret_cast<Ret (*)(Args...)>(addr)(args...);
+            }
+        };
+    } // namespace detail
+
+    template<typename Sig, typename... Args>
+    auto invoke(uintptr_t addr, Args... args) {
+        return detail::invoke_impl<Sig>::call(addr, args...);
+    }
+
+} // namespace mem
