ba263cf85022bc767233db52275e7c7fbd8f503b
PlayDay <18056374+playday3008@users.noreply.github.com>
2026-05-31 20:31:15 +0200

    feat(syndicate): add ResolutionFixHook to filter non-standard aspect ratios

--- include/games/syndicate/registry.hpp
+++ include/games/syndicate/registry.hpp
@@ -2,10 +2,11 @@
 
 #include "games/syndicate/hooks/language_unlock.hpp"
 #include "games/syndicate/hooks/refresh_rate_fix.hpp"
+#include "games/syndicate/hooks/resolution_fix.hpp"
 #include "hooks/registry/registry.hpp"
 
 namespace games::syndicate {
-    using AllHooks = hooks::hook_list<LanguageUnlockHook, RefreshRateFixHook>;
+    using AllHooks = hooks::hook_list<LanguageUnlockHook, RefreshRateFixHook, ResolutionFixHook>;
 
     using SyndicateRegistry = hooks::Registry<AllHooks>;
 
